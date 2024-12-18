97027e8929d5738e121df9a5d0e8124232f7cfe3
ian <ian.l@nexusplay.tw>
2024-12-18 13:17:55 +0800

    update: sentry

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import * as Sentry from "@sentry/vue";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+Sentry.init({
+  app,
+  dsn: "https://3cc60939706613077c4a367731cbc7d3@o4508487051902976.ingest.us.sentry.io/4508487063175168",
+  integrations: [],
+});
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
